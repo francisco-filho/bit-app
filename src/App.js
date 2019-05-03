57665ec8f6fa3c02760ff347653dacdcc1c79b04
Francisco Filho <francisco@capimgrosso.com>
2019-05-02 22:29:26 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const NEGOCIE_LIVRO_URL = "https://broker.negociecoins.com.br/api/v3/btcbrl/orde
 const BAT_URL = "https://broker.batexchange.com.br/api/v3/brleth/ticker"
 const POLONIEX = "https://poloniex.com/public?command=returnTicker"
 const MOEDAS_URL="http://68.183.139.142:3001/api/cotacoes";
-const PCT_CONVERSAO = 1.0514;
+const PCT_CONVERSAO = 1.05;
 
 const DOLAR = 3.9224
 
@@ -50,7 +50,7 @@ class App extends Component {
   valores = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
     40, 41, 42, 43, 44, 50]
     .map( v => v * 1000 + 10000)
